a89d11a159bac0ed164e0875bfb2a82a8f60f95e
okatechnology <okatechnology2020@gmail.com>
2023-10-12 12:01:19 +0900

    色々入れた

---
+++ index.js
@@ -0,0 +1,8 @@
+import eslintrcReact from './eslintrc-react';
+import prettierrc from './prettierrc';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+module.exports = {
+  eslintrcReact,
+  prettierrc,
+};
